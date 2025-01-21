82fd67bdeb22a400621cf65d6e27cd400d006855
lakshmiprasadlp <soetemp2300@gmail.com>
2025-01-21 21:42:10 +0530

    lp

--- app.py
+++ app.py
@@ -9,42 +9,44 @@ import streamlit as st
 import os
 import pathlib
 import textwrap
+from PIL import Image
+
 
 import google.generativeai as genai
 
-from IPython.display import display
-from IPython.display import Markdown
-
-
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 ## Function to load OpenAI model and get respones
 
-def get_gemini_response(question):
-    model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content(question)
+def get_gemini_response(input,image):
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    if input!="":
+       response = model.generate_content([input,image])
+    else:
+       response = model.generate_content(image)
     return response.text
 
 ##initialize our streamlit app
 
-st.set_page_config(page_title="Q&A Demo")
+st.set_page_config(page_title="Gemini Image Demo")
 
 st.header("Gemini Application")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
 
-input=st.text_input("Input: ",key="input")
 
-
-submit=st.button("Ask the question")
+submit=st.button("Tell me about the image")
 
 ## If ask button is clicked
 
 if submit:
     
-    response=get_gemini_response(input)
+    response=get_gemini_response(input,image)
     st.subheader("The Response is")
     st.write(response)
